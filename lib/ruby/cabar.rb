430e792a89d8a9e2d94f3db16d56953924db982d
kstephens <kstephens@3b289f47-807e-410a-85af-7ee1d5ead6e2>
2008-03-12 10:30:14 +0000

    Move Cabar module methods to cabar.rb.
    git-svn-id: http://cabar.rubyforge.org/svn/cabar/trunk@85 3b289f47-807e-410a-85af-7ee1d5ead6e2

--- lib/ruby/cabar.rb
+++ lib/ruby/cabar.rb
@@ -22,9 +22,59 @@
 #     component version for testing.
 #
 module Cabar
+  EMPTY_HASH = { }.freeze
+  EMPTY_ARRAY = [ ].freeze
+
+  # The Cabar version.
+  def self.version
+    Version.create '1.0'
+  end
+
+  SEMICOLON = ';'.freeze
+  COLON = ':'.freeze
+
+  # Returns the path separator for this platform.
+  # UNIX: ':'
+  # Windows: ';'
+  def self.path_sep
+    @@path_sep ||= (ENV['PATH'] =~ /;/ ? SEMICOLON : COLON)
+  end
+
+  # Split all the elements in a path.
+  # Remove any empty elements.
+  def self.path_split path, sep = nil
+    sep = path_sep
+    path = path.split(sep)
+    path.reject{|x| x.empty?}
+    path
+  end
+  
+  # Expand all the elements in a path,
+  # while leaving '@' prefixes.
+  def self.path_expand p, dir = nil
+    case p
+    when Array
+      p.map { | p | path_expand(p, dir) }.cabar_uniq_return!
+    else
+      p = p.to_s.dup
+      if p.sub!(/^@/, '')
+        '@' + File.expand_path(p, dir)
+      else
+        File.expand_path(p, dir)
+      end
+    end
+  end
+
+  # Construct a cabar YAML header.
+  def self.yaml_header str = nil
+"---
+cabar:
+  version: #{Cabar.version.to_s.inspect}
+" + (str ? "  #{str}:" : '')
+  end
 end
 
-require 'pp'
+# require 'pp'
 
 require 'cabar/array'
 require 'cabar/hash'
