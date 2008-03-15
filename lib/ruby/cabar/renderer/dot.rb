e302b60fc72193692bc3233fd4a0f3f2fff13cce
kstephens <kstephens@3b289f47-807e-410a-85af-7ee1d5ead6e2>
2008-03-15 14:46:53 +0000

    More cleanup for subgraphs.
    git-svn-id: http://cabar.rubyforge.org/svn/cabar/trunk@125 3b289f47-807e-410a-85af-7ee1d5ead6e2

--- lib/ruby/cabar/renderer/dot.rb
+++ lib/ruby/cabar/renderer/dot.rb
@@ -8,7 +8,9 @@ module Cabar
     # Renders as a dot graph.
     # See http://www.graphviz.org/
     class Dot < self
+      # Options:
       attr_accessor :show_dependencies
+      attr_accessor :group_component_versions
       attr_accessor :show_facets
       attr_accessor :show_facet_names
       attr_accessor :show_facet_links
@@ -16,6 +18,7 @@ module Cabar
       attr_accessor :show_all
 
       def initialize *args
+        @group_component_versions = false
         @show_dependencies = true
         @show_facets = false
         @show_facet_names = false
@@ -26,6 +29,7 @@ module Cabar
 
         @show_facet_links &&= @show_facets
         if @show_all
+          @group_component_versions =
           @show_dependencies =
           @show_facets =
           @show_facet_names =
@@ -70,11 +74,10 @@ module Cabar
         }.uniq.sort_by{|x| x.key}
         @facets = facets
 
-        # Delay edges until end.
+        # Delay output of edges.
         @edges = [ ]
-        @subgraph = 0
 
-        puts "digraph Cabar {"
+        puts "digraph cabar {"
         puts "  overlap=false;"
         puts "  splines=true;"
 
@@ -99,43 +102,48 @@ module Cabar
             available_components.
             select{|c| c.name == a.name }
 
-          # Show all versions available in a tooltip.
-          tooltip = "available: " + 
-            available_a.
-            sort{|a,b| a.version <=> b.version}.
-            reverse.
-            map{|v| v.version.to_s }.join(', ')
-          tooltip = tooltip.inspect
-          
-          # Are any versions of a required?
-          any_required = available_a.any?{|c| required? c}
-
-          # Make a subgraph of all versions of component a.
-          puts ""
-          puts "// #{a_name}"
-          puts "  subgraph sg_#{@subgraph += 1} {"
-          puts "    label=#{a.name.inspect};"
-          puts "    color=black;"
-          puts "    style=solid;"
- 
-          puts "    node [ shape=box, style=#{any_required ? :solid : :dotted}, label=#{"#{c_name}".inspect}, tooltip=#{tooltip} ] #{a_name};"
+          if group_component_versions
+            # Show all versions available in a tooltip.
+            tooltip = "available: " + 
+              available_a.
+              sort{|a,b| a.version <=> b.version}.
+              reverse.
+              map{|v| (required?(v) ? '*' : EMPTY_STRING) + v.version.to_s }.join(', ')
+            tooltip = tooltip.inspect
+            
+            # Are any versions of a required?
+            any_required = available_a.any?{|c| required? c}
+            
+            # Make a subgraph of all versions of component a.
+            puts ""
+            puts "// #{a_name} #{a.name}"
+            puts "  subgraph #{dot_name a, :subgraph => true} {"
+            puts "    label=#{a.name.inspect};"
+            #puts "    label=#{a.name.inspect};"
+            #puts "    color=black;"
+            #puts "    style=solid;"
+            
+            render_node a_name, 
+            :shape => :box,
+            :style => any_required ? :solid : :dotted, 
+            :label => a.name, 
+            :tooltip => tooltip
+          end
 
           versions.each do | c_v |
             render c_v
-#            
-#            b = dot_name c_v
-#            edge_puts "    #{a_name} -> #{b} [ style=dotted, arrowhead=none ];" 
-          end
-          puts "  }"
-          puts ""
-
-          edge_puts ""
-          edge_puts "// component #{a.name.inspect} versions as edges"
-          versions.each do | c_v |
-            b = dot_name c_v
-            edge_puts "    #{a_name} -> #{b} [ style=dotted, arrowhead=none ];" 
+            if group_component_versions
+              render_edge a_name, c_v, 
+              :style => :dotted, 
+              :arrowhead => :none,
+              :comment => "component #{a.name.inspect} version relationship" 
+            end
           end
 
+          if group_component_versions
+            puts "  }"
+            puts ""
+          end
         end
 
         puts ""
@@ -170,20 +178,30 @@ module Cabar
         puts "}"
       end
 
-      def render_Component c
+      def render_Component c, opts = nil
+        opts ||= EMPTY_HASH
         # $stderr.puts "render_Component #{c}"
-        required = required? c
-        style = "solid"
-        style = "dotted" unless required
-        tooltip = (c.description || c.to_s(:short)).inspect
-        puts "  node [ shape=box, label=#{dot_label c}, tooltip=#{tooltip}, style=#{style}, URL=#{('file://' + c.directory).inspect} ] #{dot_name c};"
+
+        opts = {
+          :shape => :box,
+          :label => dot_label(c),
+          :tooltip => (c.description || c.to_s(:short)),
+          :style => required?(c) ? :solid : :dotted,
+          :URL => 'file://' + c.directory,
+        }
+                 
+        if group_component_versions
+          opts[:in_subgraph] = true
+        end
+
+        render_node dot_name(c), opts
       end
 
       def render_Facet f
         # $stderr.puts "render_Facet #{f.class}"
         return unless show_facets
         return if Cabar::Facet::RequiredComponent === f
-        puts "  node [ shape=hexagon, label=#{dot_label f} ] #{dot_name f};"
+        render_node f, :shape => :hexagon, :label => dot_label(f)
       end
 
       def render_dependency_link d
@@ -196,16 +214,66 @@ module Cabar
           @components.include?(c1) &&
           @components.include?(c2)
 
-        edge_puts "  #{dot_name c1} -> #{dot_name c2, :version => false} [ style=dotted, arrowhead=open ];"
+        if group_component_versions
+          render_edge c1, dot_name(c2, :version => false),
+          :style => :dotted, 
+          :arrowhead => :open
+        end
 
-        edge_puts "  #{dot_name c1} -> #{dot_name c2} [ label=#{dot_label d}, #{required?(c1) && required?(c2) ? '' : 'style=dotted, '} arrowhead=normal ];"
-
+        render_edge c1, c2,
+        :label => dot_label(d),
+        :arrowhead => :normal,
+        :style => required?(c1) && required?(c2) ? nil : :dotted
       end
 
       def render_facet_link c, f
         return if Cabar::Facet::RequiredComponent === f
         return unless show_facet_links
-        edge_puts "  #{dot_name c} -> #{dot_name f} [ style=dotted, arrowhead=none ];"
+        render_edge c, f, :style => :dotted, :arrowhead => :none
+      end
+
+      def render_node name, opts = nil
+        opts ||= EMPTY_HASH
+        name = dot_name(name) unless String === name
+        prefix = name
+        suffix = EMPTY_STRING
+
+        if true || opts[:in_subgraph]
+          opts.delete(:in_subgraph) rescue nil
+          prefix = 'node'
+          suffix = name
+        end
+
+        if opts[:comment]
+          edge_puts "// #{opts[:comment]}"
+          opts.delete(:comment)
+        end
+
+        puts "    #{prefix} #{dot_opts opts} #{suffix};"
+      end
+
+      def render_edge n1, n2, opts = nil
+        opts ||= EMPTY_HASH
+
+        n1 = dot_name(n1) unless String === n1
+        n2 = dot_name(n2) unless String === n2
+
+        if opts[:comment]
+          edge_puts "// #{opts[:comment]}"
+          opts.delete(:comment)
+        end
+
+        edge_puts "    #{n1} -> #{n2} #{dot_opts opts};"
+      end
+
+      # Options with nil values are not expanded.
+      def dot_opts opts = nil
+        opts ||= EMPTY_HASH
+        unless opts.empty?
+          "[ #{opts.map{|k, v| v.nil? ? nil : "#{k}=#{v.to_s.inspect.gsub(/\\\\/, '\\')}"}.compact.join(', ')} ]"
+        else
+          EMPTY_STRING
+        end
       end
 
       def required? c
@@ -226,25 +294,26 @@ module Cabar
         @dot_name[[ x, opts ]] ||=
           case x
           when Cabar::Component
-            prefix = ''
+            prefix = EMPTY_STRING
             if opts[:subgraph]
               opts[:version] = false
-              prefix = "SG "
+              prefix = "s"
             end
 
             prefix +
             case opts[:version]
             when false
-              "C #{x.name}"
+              "c #{x.name}"
             else
-              "C #{x.name} #{x.version}"
+              "c #{x.name} #{x.version}"
             end
           when Cabar::Facet
-            "F #{x.key}"
+            "f #{x.key}"
           else
-            "X #{x}"
+            "x #{x}"
           end.
-          inspect
+          sub(/([a-z]+) (.*)/i){|| "#{$1}_#{$2.hash}"}.
+          gsub('-', '_') 
       end
 
 
@@ -260,9 +329,9 @@ module Cabar
             str = ''
             str << "#{x.name}"
             str << " #{x.version}" if opts[:version] != false
-            str << "\\n#{dir}"
+            str << "\n#{dir}"
             if show_facet_names && opts[:show_facet_names] != false
-              str << "\\n"
+              str << "\n"
               # <- <<exported facet name>>
               x.provides.
                 map{|f| f.key}.
@@ -277,15 +346,16 @@ module Cabar
                 uniq.
                 each{|p| str << "<* #{p}\\l"}
             end
-            '"' + str + '"'
+            # '"' + str + '"'
+            str
           when Cabar::Facet::RequiredComponent
             # Use the version requirement.
-            x._proto ? "#{x.version}".inspect : x.key.to_s.inspect
+            x._proto ? "#{x.version}" : x.key
           when Cabar::Facet
             # Use the facet name.
-            x.key.to_s.inspect
+            x.key.to_s
           else
-            x.to_s.inspect
+            x.to_s
           end
       end
 
