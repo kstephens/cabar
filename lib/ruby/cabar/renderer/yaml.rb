ec83ac0712328dca33e2d54b850a64e3d1beec93
kstephens <kstephens@3b289f47-807e-410a-85af-7ee1d5ead6e2>
2008-03-15 01:07:58 +0000

    Moved Renderer::Yaml to separate file.
    git-svn-id: http://cabar.rubyforge.org/svn/cabar/trunk@123 3b289f47-807e-410a-85af-7ee1d5ead6e2

---
+++ lib/ruby/cabar/renderer/yaml.rb
@@ -0,0 +1,115 @@
+require 'cabar/renderer'
+
+
+module Cabar
+
+  class Renderer
+
+    # Renders objects as YAML.
+    # TODO: make it smarter and leaner.
+    class Yaml < self
+      attr_accessor :verbose
+
+      def initialize *args
+        @verbose = false
+        super
+      end
+
+
+      # Only render the header once.
+      def render_header
+        return if @render_header
+        @render_header = 1
+        puts Cabar.yaml_header
+      end
+
+      def render_components comps, opts = EMPTY_HASH
+        render_header
+        puts "  component: "
+        comps.
+        sort { | a, b | 
+          (x = a.name <=> b.name) != 0 ? x :
+          (x = b.version <=> a.version) != 0 ? x :
+          0
+        }.
+        each do | c |
+          render c, opts
+        end
+      end
+      
+      def render_facets facets, opts = EMPTY_HASH
+        x = opts[:indent]
+        x ||= ''
+        render_header
+        puts "#{x}  facet: "
+
+        facets = facets.
+          sort { | a, b | a.key <=> b.key }
+
+        facets.each do | facet |
+          render facet, opts
+        end
+      end
+
+      def render_Component c, opts = EMPTY_HASH
+        if @verbose 
+          puts "  - name:          #{c.name.inspect}"
+          puts "    version:       #{c.version.to_s.inspect}"
+          puts "    description:   #{c.description.inspect}" if c.description
+          puts "    directory:     #{c.directory.inspect}"
+          puts "    base_dir:      #{c.base_directory.inspect}" if c.base_directory != c.directory
+          puts "    facet:         [ #{c.provides.map{|x| x.key.inspect}.sort.join(', ')} ]"
+          puts "    requires:      [ #{c.requires.map{|x| "#{x.name}/#{x.version}".inspect}.sort.join(', ')} ]"
+          puts "    configuration: #{c.configuration.inspect}" if ! c.configuration.empty?
+          puts "    plugins:       #{c.plugins.map{|p| p.name}.inspect}" if ! c.plugins.empty?
+          render_facets c.facets, :indent => '  ' if _options[:show_facet]
+          puts ""
+        else
+          puts "  - #{[ c.name, c.version.to_s, c.directory ].inspect}"
+        end
+      end
+
+      def render_Facet facet, opts = EMPTY_HASH
+        x = opts[:indent]
+        x ||= ''
+
+        case 
+        when @verbose
+          case
+          when opts[:prototype]
+            puts "#{x}    #{facet.key}:"
+            puts "#{x}      class:       #{facet.class.to_s}"
+            puts "#{x}      _defined_in: #{facet._defined_in.to_s.inspect}"
+          else            
+            a = facet.to_a
+            a << [ :_defined_in, facet._defined_in.to_s ]
+            puts "#{x}  - " + 
+              (a.
+               map do | k, v |
+                 case v
+                 when Hash
+                   str = ''
+                   v.each do | vk, vv |
+                     str << "\n#{x}      #{vk}: #{vv.inspect}"
+                   end
+                   v = str
+                 else
+                   v = v.inspect
+                 end
+                 "#{k}: #{v}"
+               end.
+               join("\n#{x}    ")
+               )
+            puts ""
+          end
+        else
+          puts "#{x}  - #{facet.key.inspect}"
+        end
+      end
+      
+    end # class
+
+  end # class
+  
+end # module
+
