fed60bcb7dd0b8cdaaa82312690f685424b1c37a
kstephens <kstephens@3b289f47-807e-410a-85af-7ee1d5ead6e2>
2008-03-23 02:52:48 +0000

    Preliminary "cbr gems gem install" command.
    git-svn-id: http://cabar.rubyforge.org/svn/cabar/trunk@170 3b289f47-807e-410a-85af-7ee1d5ead6e2

--- comp/rubygems/cabar.rb
+++ comp/rubygems/cabar.rb
@@ -11,7 +11,7 @@ DOC
   cmd_group [ :rubygems, :gems ] do
 
     cmd [ :list ] , <<'DOC' do
-[ - <component> ] [ <action> ] 
+[ - <component> ]
 List gems repositories.
 DOC
       select_root cmd_args
@@ -31,11 +31,56 @@ DOC
       end
     end
 
+    cmd :gem , <<'DOC' do
+[ - <gems-component> ] <<gem-cmd-args>> 
+Run gem using a gems component environment
+
+Example:
+  cbr gems gem install - my_gems_component rails
+
+Installs "rails" gem into "my_gems_component/gems".
+
+DOC
+      root = select_root cmd_args
+
+      gem_home = ENV['GEM_HOME']
+      gem_path = ENV['GEM_PATH']
+
+      print_gem_env "Before setup_environment!"
+
+      # Render environment vars.
+      setup_environment!
+
+      print_gem_env "After setup_environment!"
+
+      get_gems_facets(root).each do | c, facet |
+        begin
+          ENV['GEM_HOME'] = facet.abs_path.first
+          ENV['GEM_PATH'] = Cabar.path_join(facet.abs_path, gem_path)
+          print_gem_env 'For gem'
+          system('gem', *cmd_args)
+        ensure
+          ENV['GEM_HOME'] = gem_home
+          ENV['GEM_PATH'] = gem_path
+        end
+      end
+    end
+
+
     class Cabar::Command
+      def print_gem_env header = nil
+        puts "\n#{header}:" if header
+        [ :RUBYLIB, :GEM_HOME, :GEM_PATH ].each do | v |
+          v = v.to_s
+          puts "  #{v}=#{ENV[v].inspect}" if ENV[v]
+        end
+      end
+
       def get_gems_facets match = nil
         result = [ ]
         
         context.required_components.each do | c |
+          next if match && ! (match === c)
           # puts "c.facets = #{c.facets.inspect}"
           c.facets.each do | f |
             if f.key == 'rubygems' &&
--- example/repo/plat/gems/1.0/cabar.rb
+++
@@ -1,5 +0,0 @@
-Cabar::Facet::EnvVarPath.
-  new(:key => :gems, 
-      :var => :GEM_PATH,
-      :std_path => :gems
-      ).register_prototype!
