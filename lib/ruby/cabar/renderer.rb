03dc4d05cf22143d0cb9a51e84d3e3d24b5d6395
Kurt Stephens <ks.github@kurtstephens.com>
2009-06-17 15:38:33 -0500

    Refactored render_* methods into Cabar::Multimethod. Task 63502

--- lib/ruby/cabar/renderer.rb
+++ lib/ruby/cabar/renderer.rb
@@ -1,11 +1,14 @@
 require 'cabar/base'
 
+require 'cabar/multimethod'
 
 module Cabar
 
   # Base class for rendering methods of Components and Facets.
   class Renderer < Base
-    # Controlls how verbose output should be.
+    include Cabar::Multimethod
+
+    # Controls how verbose output should be.
     attr_accessor :verbose
     
     # The IO object for puts, defaults to $stdout.
@@ -30,110 +33,8 @@ module Cabar
     end
 
 
-    # Multimethod dispatching based on first argument's
-    # ancestors.
-    #
-    # For example:
-    # 
-    #   renderer.render 4, :xyz
-    #
-    # Would attempt to send:
-    #
-    #   renderer.render_Fixnum 4, :xyz
-    #   renderer.render_Integer 4, :xyz
-    #   renderer.render_Precision 4, :xyz
-    #   renderer.render_Numeric 4, :xyz
-    #   renderer.render_Comparable 4, :xyz
-    #   renderer.render_Object 4, :xyz
-    #
-    # Kernel is avoided and namespaces are removed from ancestor names.
-    def render x, *args
-      x.class.ancestors.each do | cls |
-        next if cls == Kernel
-        meth = :"render_#{cls.name.sub(/^.*::/, '')}" 
-        return send(meth, x, *args) if respond_to? meth
-      end
-      raise ArgumentError, "Cannot find render_* for #{x.class}"
-    end
-
-
-    # Multimethod dispatching based on common ancestors
-    # of all elements in the Array.
-    #
-    # For example:
-    # 
-    #   x = [ 4, 4.5 ]
-    #   renderer.render x, :xyz
-    #
-    # Would send to the first method where self.respond_to? is true.
-    #
-    #   renderer.render_Array_of_Precision x, :xyz
-    #   renderer.render_Array_of_Numeric x, :xyz
-    #   renderer.render_Array_of_Comparable x, :xyz
-    #   renderer.render_Array_of_Object x, :xyz
-    #
-    # Kernel is avoided and namespaces are removed from ancestor names.
-    def render_Array x, *args
-      # Get a set of common ancestors of all element values.
-      val_ancestors = x.inject(x.first.class.ancestors) do | a, xi | 
-        a & xi.class.ancestors
-      end
-      val_ancestors.delete(::Kernel)
-
-      # Find first method for ancestor named "render_Array_<<ancestor>>".
-      val_ancestors.each do | cls |
-        meth = :"render_Array_of_#{cls.name.sub(/^.*::/, '')}" 
-        return send(meth, x, *args) if respond_to? meth
-      end
-
-      raise ArgumentError, "Cannot find render_Array_of_* for #{x.class} using common ancestors #{val_ancestors.inspect}"
-    end
-
-
-    # Multimethod dispatching based on common ancestors
-    # of all elements in the Hash.
-    #
-    # For example:
-    # 
-    #   x = { :a=> 4, :b => 4.5 }
-    #   renderer.render x, :xyz
-    #
-    # Would send to the first method where self.respond_to? is true.
-    #
-    #   renderer.render_Hash_of_Symbol_and_Precision x, :xyz
-    #   renderer.render_Hash_of_Symbol_and_Numeric x, :xyz
-    #   renderer.render_Hash_of_Symbol_and_Comparable x, :xyz
-    #   renderer.render_Hash_of_Symbol_and_Object x, :xyz
-    #   renderer.render_Hash_of_Object_and_Precision x, :xyz
-    #   renderer.render_Hash_of_Object_and_Numeric x, :xyz
-    #   renderer.render_Hash_of_Object_and_Comparable x, :xyz
-    #   renderer.render_Hash_of_Object_and_Object x, :xyz
-    #
-    # Kernel is avoided and namespaces are removed from ancestor names.
-    def render_Hash x, *args
-      # Get a set of common ancestors of all key elements.
-      key_ancestors = x.keys.inject(x.keys.first.class.ancestors) do | a, xi | 
-        a & xi.class.ancestors
-      end
-      key_ancestors.delete(::Kernel)
-
-      # Get a set of common ancestors of all value elements.
-      val_ancestors = x.values.inject(x.values.first.class.ancestors) do | a, xi | 
-        a & xi.class.ancestors
-      end
-      val_ancestors.delete(::Kernel)
-
-      # Find first method for ancestor named "render_Hash_of_<<key_ancestor>>_and_<<val_ancestor>>".
-      key_ancestors.each do | key_cls |
-        val_ancestors.each do | val_cls |
-          meth = :"render_Hash_of_#{key_cls.name.sub(/^.*::/, '')}_and__#{val_cls.name.sub(/^.*::/, '')}" 
-          return send(meth, x, *args) if respond_to? meth
-        end
-      end
-
-      raise ArgumentError, "Cannot find render_Hash_of_*_and_* for #{x.class} using common key ancestors #{key_ancestors.inspect} and value ancestors #{val_ancestors.inspect}"
-    end
-
+    # Define render_* dispatching methods.
+    multimethod :render
 
     # Default Selection rendering method.
     # Renders via #render_Array_of_Component.
